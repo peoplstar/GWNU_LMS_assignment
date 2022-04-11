5bbaba6efc66a58d4bd8014a2b88ae36118f506b
peoplstar <asxz3202@naver.com>
2022-04-12 00:10:10 +0900

    Update firebaseLink.py

--- firebaseLink.py
+++ firebaseLink.py
@@ -2,9 +2,16 @@
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 from firebase_admin import db
 
 # Firebase database 인증 및 앱 초기화
+db_url = 'https://lms-assignment-default-rtdb.firebaseio.com/'
+
 cred = credentials.Certificate("lms-assignment-firebase-adminsdk-gg9hv-0e2b022f8b.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : db_url
+})
 
-
+ref = db.reference('')
+print(ref.get())
