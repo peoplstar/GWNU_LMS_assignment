8f2227b856e8fce737c8773eed8ccd8572fb4787
peoplstar <asxz3202@naver.com>
2022-04-18 17:06:05 +0900

    in the event of login failed

--- lms_assignment/api_app/views.py
+++ lms_assignment/api_app/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import lmsItemSerializer
 from .models import lmsItem
+from selenium.common.exceptions import UnexpectedAlertPresentException as PE # 로그인 오류
+
 import Pldd
 import json
 import firebaseLink
@@ -28,15 +30,27 @@ class lmsItemViews(APIView):
             
             # Crawling Method Parameter 
             crawSystem = Pldd.crawling(userid, password)
-            txt = crawSystem.craw()
+            
+            try :
+                txt = crawSystem.craw()
+                status_code = 200
+            except PE :
+                txt = 'Login Failed'
+                status_code = 400
+                
             
             # JSON DB data processing
             firedb = firebaseLink.DBLink(userid)
             firedb.rwJson("")
             firedb.Link("")
-
-            return Response(txt)
-        #({"status" : "success", "data" : serializer.data}, status = status.HTTP_200_OK)
+            
+            if status_code == 200:
+                return Response(txt, status = status.HTTP_200_OK)
+            else:
+                return Response({"data" : txt}, status = status.HTTP_400_BAD_REQUEST)
+        #()
+        '''
         else:
             return Response("Login Failed")
-        #{"status " : "Login Failed", "data" : serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+        {"status " : "Login Failed", "data" : serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+        '''
