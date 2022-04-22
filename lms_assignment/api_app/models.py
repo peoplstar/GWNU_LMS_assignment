c8243da12d484ec30cf0cad560e7b4ccd845e71d
peoplstar <asxz3202@naver.com>
2022-04-22 17:54:10 +0900

    Firebase Token API

--- lms_assignment/api_app/models.py
+++ lms_assignment/api_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class lmsItem(models.Model):
     lms_id = models.CharField(max_length = 15, primary_key = True)
     lms_pw = models.CharField(max_length = 30)
-
+    # FirebaseToken = models.CharField(max_length = )
 """
 class assignment(models.Model):
     course = models.CharField(max_length = 20)
