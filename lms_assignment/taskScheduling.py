492451678982fb616383abc1b454fc07867083de
peoplstar <asxz3202@naver.com>
2022-04-22 17:53:32 +0900

    Task Scheduling
    cran AM 6 : 00

---
+++ lms_assignment/taskScheduling.py
@@ -0,0 +1,43 @@
+# pip install firebase_admin
+
+import json
+import firebase_admin
+import Pldd
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
+
+# 오전 6시 Crawling
+# 비교 전 제출 기한 over delete
+# Firebase ID / PW 접근 후 Pldd
+# DB & JSON 비교
+
+class taskScheduling:
+    def access():
+        db_url = 'https://lms-assignment-default-rtdb.firebaseio.com/'
+
+        if not firebase_admin._apps:
+            cred = credentials.Certificate("Firebase.json")
+            firebase_admin.initialize_app(cred, {
+                'databaseURL' : db_url
+            })
+        
+        ref = db.reference('')
+        key_list = (ref.get().keys()) # 학번만 출력 ref.get().keys()
+
+        for id in key_list:
+            userid = db.reference(id + '/passwd')   
+            passwd = userid.get() # 복호화 과정 필요
+            
+            craws = Pldd.crawling(userid, passwd)
+            craws.craw()
+            '''
+            Crawling 된 JSON을 Firebase와 비교
+            '''
+            # print(f'userid : {id} passwd : {passwd}')
+            
+            
+            
+            
+if __name__ == "__main__":
+    taskScheduling.access()
