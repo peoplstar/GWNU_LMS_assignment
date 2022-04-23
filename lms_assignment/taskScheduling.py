3c1c38bcbfe7e9292d385f22ab08bec8fca188ef
peoplstar <asxz3202@naver.com>
2022-04-23 18:31:21 +0900

    Update taskScheduling.py

--- lms_assignment/taskScheduling.py
+++ lms_assignment/taskScheduling.py
@@ -6,12 +6,16 @@ import Pldd
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import db
-
+from datetime import datetime
 # 오전 6시 Crawling
 # 비교 전 제출 기한 over delete
 # Firebase ID / PW 접근 후 Pldd
 # DB & JSON 비교
-
+def timeControl():
+    now = datetime.now()
+    print(now.strftime('%Y-%m-%d %H:%M:%S'))
+    # return 타임 비교 값
+    
 class taskScheduling:
     def access():
         db_url = 'https://lms-assignment-default-rtdb.firebaseio.com/'
@@ -24,18 +28,21 @@ class taskScheduling:
         
         ref = db.reference('')
         key_list = (ref.get().keys()) # 학번만 출력 ref.get().keys()
-
+        # timeControl()
+        d_day_end = db.reference('20171473')
+        print(d_day_end)
+        '''
         for id in key_list:
             userid = db.reference(id + '/passwd')   
             passwd = userid.get() # 복호화 과정 필요
             
             craws = Pldd.crawling(userid, passwd)
             craws.craw()
-            '''
+            
+            
             Crawling 된 JSON을 Firebase와 비교
             '''
             # print(f'userid : {id} passwd : {passwd}')
-            
             
             
             
