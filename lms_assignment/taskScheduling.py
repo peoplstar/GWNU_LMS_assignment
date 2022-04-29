fdb237d8ecf34d30e5a07afd28ad16ecb14d0336
peoplstar <asxz3202@naver.com>
2022-04-29 13:30:09 +0900

    Task's Time Output

--- lms_assignment/taskScheduling.py
+++ lms_assignment/taskScheduling.py
@@ -2,24 +2,27 @@
 
 import json
 from time import strptime
+from turtle import st
 import firebase_admin
 import Pldd
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import db
 from datetime import datetime
+
 # 오전 6시 Crawling
 # 비교 전 제출 기한 over delete
 # Firebase ID / PW 접근 후 Pldd
 # DB & JSON 비교
+
 def timeControl(d_day_end):
     #self.d_day_end = d_day_end
     now = datetime.now()
     now = now.strftime('%Y-%m-%d %H:%M')
     now = datetime.strptime(now, '%Y-%m-%d %H:%M')
-    print(f'd_day_end = {d_day_end}')
-    diff = d_day_end - now
-    print(d_day_end> now) # d_day_end 가 now 보다 과거일 경우 False
+    # print(f'd_day_end = {d_day_end}')
+
+    print(d_day_end > now) # d_day_end 가 now 보다 과거일 경우 False
 
     # return 타임 비교 값
     
@@ -34,13 +37,26 @@ class taskScheduling:
             })
         
         ref = db.reference('')
-        key_list = (ref.get().keys()) # 학번만 출력 ref.get().keys()
-        
-        # timeControl()
-        d_day_end = db.reference('20171456/task/0/d_day_end').get() # Time key
-        d_day_end = datetime.strptime(d_day_end, '%Y-%m-%d %H:%M')
+        dict_key_list = ref.get().keys() # 학번만 출력 ref.get().keys()
+        key_list = list(dict_key_list) # 학번 List 변환
+        task_list = db.reference(key_list[0] + '/task').get()
+        tmp = db.reference(key_list[0] + '/task/0').get()
+
+        for task in range(0, len(task_list)):
+            r = db.reference(key_list[0] + '/task/' + str(task) + '/d_day_end') # 과제 별 마감 시간
+            print(r.get())
+            
+        # for key in key_list:
+        #     for task in task_list:
+        #         time = db.reference(key + task + '/d_day_end').get()
+        #         ret = timeControl(time)
+        #         if ret == False :
+
+        #d_day_end = db.reference('20171456/task/0/d_day_end').get() # Time key
+        #d_day_end = datetime.strptime(d_day_end, '%Y-%m-%d %H:%M')
         #print(type(d_day_end)) # Time value
-        timeControl(d_day_end)
+        #timeControl(d_day_end)
+        
         '''
         for id in key_list:
             userid = db.reference(id + '/passwd')   
