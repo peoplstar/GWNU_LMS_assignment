896448490934f2cb1da0ab0bfcdf7812a8d67156
peoplstar <asxz3202@naver.com>
2022-05-07 18:08:43 +0900

    Hiding Certification File Name

--- lms_assignment/taskScheduling.py
+++ lms_assignment/taskScheduling.py
@@ -15,22 +15,27 @@ from datetime import datetime
 # Firebase ID / PW 접근 후 Pldd
 # DB & JSON 비교
 
+data = {}
+
 def timeControl(d_day_end):
-    #self.d_day_end = d_day_end
     now = datetime.now()
     now = now.strftime('%Y-%m-%d %H:%M')
     now = datetime.strptime(now, '%Y-%m-%d %H:%M')
 
     return (d_day_end > now) # d_day_end 가 now 보다 과거일 경우 False
 
-    # return 타임 비교 값
+def rwJson(key): 
+    with open('./assignmentJson/' + key + '.json', 'r+', encoding = "UTF-8") as f: 
+        tmp = json.load(f)
+        data = tmp
+        return len(data['task'])
     
 class taskScheduling:
     def access():
         db_url = 'https://lms-assignment-default-rtdb.firebaseio.com/'
 
         if not firebase_admin._apps:
-            cred = credentials.Certificate("./lms-assignment-firebase-adminsdk-gg9hv-0e2b022f8b.json")
+            cred = credentials.Certificate("Firebase.json")
             firebase_admin.initialize_app(cred, {
                 'databaseURL' : db_url
             })
@@ -38,18 +43,28 @@ class taskScheduling:
         ref = db.reference('')
         dict_key_list = ref.get().keys() # 학번 dictionary's keys
         key_list = list(dict_key_list) # 학번 List 변환
-        task_list = db.reference(key_list[0] + '/task').get()
-
-        for task in range(0, len(task_list)):
-            r = db.reference(key_list[0] + '/task/' + str(task) + '/d_day_end') # 과제 별 마감 시간
-            timeResult = timeControl(datetime.strptime(r.get(), '%Y-%m-%d %H:%M')) # 과제 마감 시간
+        
+        # DB에 저장된 ID와 PW로 크롤링 한 JSON 파일 로딩
+        
+        for key in key_list:
+            task_list = db.reference(key + '/task').get()
+            taskCnt = len(list(filter(None, task_list))) # Remove None data in List
             
-            # 시간 값 비교 해당 과제 삭제
-            if timeResult == False:
-                deleteItem = db.reference(key_list[0] + '/task/' + str(task))
-                print(f'Time Result == {timeResult}')
-                deleteItem.delete()
+            for task in range(0, taskCnt):
+                r = db.reference(key + '/task/' + str(task) + '/d_day_end') # 과제 별 마감 시간
+                timeResult = timeControl(datetime.strptime(r.get(), '%Y-%m-%d %H:%M'))
                 
+                # 시간 값 비교 해당 과제 삭제
+                if timeResult == False:
+                    deleteItem = db.reference(key + '/task/' + str(task))
+                    deleteItem.delete()
+                    taskCnt -= 1
+            
+            pw = db.reference(key + '/pw').get() # DB id, pw Link
+            crawSystem = Pldd.crawling(key, pw)
+             
+            newTask = crawSystem.craw()
+            newTaskCnt = rwJson(key)
 
         # d_day_end = db.reference('20171456/task/0/d_day_end').get() # Time key
         # d_day_end = datetime.strptime(d_day_end, '%Y-%m-%d %H:%M')
@@ -67,9 +82,9 @@ class taskScheduling:
             
             Crawling 된 JSON을 Firebase와 비교
         '''
-            # print(f'userid : {id} passwd : {passwd}')
-            
-            
-            
-if __name__ == "__main__":
+        
+def main():
     taskScheduling.access()
+    
+if __name__ == "__main__":
+    main()
