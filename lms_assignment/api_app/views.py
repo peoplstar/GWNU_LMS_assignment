7b666ef1358a7dd76bda07a34a2fdaa1f93792f8
peoplstar <asxz3202@naver.com>
2022-05-09 20:33:53 +0900

    Decrease Overhead

--- lms_assignment/api_app/views.py
+++ lms_assignment/api_app/views.py
@@ -5,41 +5,64 @@ from rest_framework import status
 from .serializers import lmsItemSerializer
 from .models import lmsItem
 from selenium.common.exceptions import UnexpectedAlertPresentException as PE # 로그인 오류
-
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
 import Pldd
 import json
 import firebaseLink
+import firebase_admin
+
 # from .models import assignment
 # Create your views here.
 
+def isNone(r):
+    if r.get() == None:
+        return 'isNone'
+    else:
+        return 'isNotNone'
+    
 class lmsItemViews(APIView):
     '''
     def get(self, request, lms_id):
         return(RSA 공개키를 전달)
     '''
 
-
-
     def post(self, request):
         serializer = lmsItemSerializer(data = request.data)
         if serializer.is_valid():
             # serializer.save()
+
+            ref = db.reference('')
+            dict_key_list = ref.get().keys() # 학번 dictionary's keys
+            key_list = list(dict_key_list) # 학번 List 변환
             inform = request.data
             dump = json.dumps(inform)
             tmp = json.loads(dump)
             userid = tmp['lms_id']
             password = tmp['lms_pw']
-            
-            # Crawling Method Parameter 
-            crawSystem = Pldd.crawling(userid, password)
-            
-            try :
-                txt = crawSystem.craw()
-                status_code = 200
-            except PE :
-                txt = 'Login Failed'
-                status_code = 400
+            db_url = 'https://lms-assignment-default-rtdb.firebaseio.com/'
+
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("./lms-assignment-firebase-adminsdk-gg9hv-0e2b022f8b.json")
+                firebase_admin.initialize_app(cred, {
+                    'databaseURL' : db_url
+                })
                 
+            ref = db.reference(userid)
+            if isNone(ref) == isNone:
+                # Crawling Method Parameter 
+                crawSystem = Pldd.crawling(userid, password)
+                
+                try :
+                    txt = crawSystem.craw()
+                    status_code = 200
+                except PE :
+                    txt = 'Login Failed'
+                    status_code = 400
+            else:
+                r = ref.child('task')
+                txt = r.get()
             
             # JSON DB data processing
             firedb = firebaseLink.DBLink(userid)
