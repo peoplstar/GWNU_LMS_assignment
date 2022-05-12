65974a7adb85e837225b7f7ee6c06327938265bd
peoplstar <asxz3202@naver.com>
2022-05-12 18:45:49 +0900

    Local Variable

--- lms_assignment/push_fcm_notification.py
+++ lms_assignment/push_fcm_notification.py
@@ -1,19 +1,18 @@
 from pyfcm import FCMNotification
  
-APIKEY = "Your Server Key"
+APIKEY = "AAAAmflPfQQ:APA91bEuJalspiK5RlQ-x5LN1zgpcfhO3mZNbDRMIf-DuuksI8zDGwduvMEpjk8-45sj06AnOMyoDL7CEYx-G3lFVj-UM5Kp2RQQzQxfzOBg5D0JRgQv8CjqEoOM7nkY7usEit5YZLK6"
  
 # 파이어베이스 콘솔에서 얻어 온 서버 키를 넣어 줌
 push_service = FCMNotification(APIKEY)
  
-def sendMessage(body, title, local_token):
+def sendMessage(self, body, title, token):
     # 메시지 (data 타입)
-    global token
-    token = local_token
+    self.token = token
     
     data_message = {
-        "body": body,
-        "title": title
+        "body" : body,
+        "title" : title
     }
  
     # 토큰값을 이용해 1명에게 푸시알림을 전송함
-    push_service.single_device_data_message(registration_id = token, data_message = data_message)
+    push_service.single_device_data_message(registration_id = self.token, data_message = data_message)
